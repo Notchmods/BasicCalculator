2e1a113af23c8825ddfca2a2a55600654433dfba
Notchmods <Nomics3000@gmail.com>
2020-04-15 17:25:47 +1000

    The non zip version of the calc pls delete the zip file if you don't want it

---
+++ Simplecalculator/Simplecalculator/Simplecalculator/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Simplecalculator
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(72, 12);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(364, 111);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(72, 251);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(364, 111);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(72, 494);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(364, 111);
+			this.textBox3.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(211, 390);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 64);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "=";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(145, 158);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(214, 74);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "+";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+			this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+			// 
+			// Form
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(572, 617);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "Form";
+			this.Text = "Simple Calculator";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Simplecalculator/Simplecalculator/Simplecalculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿//Those are components
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simplecalculator 
+{
+	//These codes are copyright free 
+    public partial class Form : System.Windows.Forms.Form
+    {
+		//Upon intialization  
+        public Form()
+        {
+			//Initialize the forms
+            InitializeComponent();
+        }
+
+		//Button onclick 
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//Check the user inserted a legit numbers.
+			try
+			{ 
+				//Analyze the numbers
+				float jepht = float.Parse(textBox1.Text);
+				float Jeam = float.Parse(textBox2.Text);
+				//float - Define a variables with a fractional value example float = 1.756 , int = 1 
+				//Add or subtract or multiply or divide the numbers into one a single float
+				float Jones = jepht + Jeam;
+				textBox3.Text = Jones.ToString();
+			}
+			//If it isn't a number then this message will show up
+			catch
+			{
+				MessageBox.Show("That's not a number enter a legit number");
+			}
+		}
+
+		//When the mouse touch the button the buttons background color will turn to any color other then its original
+		private void button1_MouseEnter(object sender, EventArgs e)
+		{  
+			button1.BackColor = Color.Yellow;
+		}
+
+		//If the mouse leaves the button the button will return to its original color.
+		private void button1_MouseLeave(object sender, EventArgs e)
+		{
+			button1.BackColor = Color.Transparent;
+		}
+	}
+}
